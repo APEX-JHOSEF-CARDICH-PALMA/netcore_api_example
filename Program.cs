0bc003a2edd9bfb12cfa377760a384b0d45d7f55
Jhosef <jacp.tech.91@gmail.com>
2020-10-09 14:37:04 +0200

    starting main development branch

--- Program.cs
+++ Program.cs
@@ -16,11 +16,12 @@ namespace netcore_api_example
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-    }
+    }ee
 }
